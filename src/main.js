bd3fb0899f2f040fcaf75909eaa93d6c17bb4b64
adrian651 <adrianrh651@gmail.com>
2019-07-14 18:45:51 -0700

    First stages

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import home from './components/home.vue';
 
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: home },
+];
+
+const router = new VueRouter({
+  routes,
+  mode:'history'
+})
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
